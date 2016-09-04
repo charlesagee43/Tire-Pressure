baeefea14ac144d51ea01a3159965cc9393f486e
Charles Agee <Charles Agee@CJA-PC>
2016-09-03 20:30:04 -0500

    Added Code

---
+++ TirePressure/TirePressure/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TirePressure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello" + " " + firstName + " " + lastName);
+
+            Console.ReadLine();
+        }
+    }
+}
